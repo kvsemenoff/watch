76aac3adac6f9bd1fe7445a3d574d10607da7eb6
Berzeg07 <berezg86@gmail.com>
2017-01-10 16:21:18 +0300

    29.14

--- includes/header.php
+++ includes/header.php
@@ -1,15 +1,31 @@
-<section class="section section-dd">
+<section class="section section_view-dd section_padding-dd hidden-xs hidden-sm">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<div class="nav">
-					<a href="#">Модели</a>
-					<a href="#">Гарантии безопасности</a>
-					<a href="#">Оплата и доставка</a>
-					<a href="#">Сертификаты</a>
-				</div>
+				<ul class="main-menu main-menu_position">
+					<li class="main-menu__item main-menu__item_view"><a class="main-menu__link main-menu__link_view" href="#">Модели</a></li>
+					<li class="main-menu__item main-menu__item_view"><a class="main-menu__link main-menu__link_view" href="#">Гарантии безопасности</a></li>
+					<li class="main-menu__item main-menu__item_view"><a class="main-menu__link main-menu__link_view" href="#">Оплата и доставка</a></li>
+					<li class="main-menu__item main-menu__item_view"><a class="main-menu__link main-menu__link_view" href="#">Сертификаты</a></li>
+				</ul>
 			</div>
 			<div class="clear"></div>	
+		</div> <!-- row -->
+	</div> <!-- container -->
+</section> <!-- section_view-dd -->
+
+<div class="anz-menu hidden-lg hidden-md">
+	<input type="checkbox" id="check_1" class=""/>
+	<label class="anz-menu-ch " for="check_1"><i class="fa fa-bars" aria-hidden="true"></i><img src="img/anz.png"></label>
+	<div class="anz-perspective">
+		<div class="menu-list-pressed">
+			<label class="anz-menu-ch1" for="check_1"><i class="fa fa-times" aria-hidden="true"></i><img src="img/kres.png"></label>
+			<ul class="main-menu main-menu_position">
+				<li class="main-menu__item main-menu__item_view"><a class="main-menu__link main-menu__link_view" href="#">Модели</a></li>
+				<li class="main-menu__item main-menu__item_view"><a class="main-menu__link main-menu__link_view" href="#">Гарантии безопасности</a></li>
+				<li class="main-menu__item main-menu__item_view"><a class="main-menu__link main-menu__link_view" href="#">Оплата и доставка</a></li>
+				<li class="main-menu__item main-menu__item_view"><a class="main-menu__link main-menu__link_view" href="#">Сертификаты</a></li>
+			</ul>
 		</div>
 	</div>
-</section>
+</div>

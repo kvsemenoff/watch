b150dcdb1ef6f1f4de72f2876e52b4c194cbe899
unknown <anzarsh@mail.ru>
2017-01-10 17:12:58 +0300

    add

---
+++ includes/status_clocks.php
@@ -0,0 +1,17 @@
+<section class="section section_padding-db section_view-db">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="db-status db-status_view db-status_padding">
+					<span class="db-status__title db-status__title_view">
+						СТАТУСНЫЕ ЧАСЫ ДЛЯ<br>
+						ПАТРИОТОВ НАШЕЙ СТРАНЫ.
+					</span>
+					<div class="db-status__text db-status__text_view">
+						Часы серии «Казахстан» разрабатывались специально для вручения особым людям за проявленный патриотизм к нашей Родине. Часы выглядят не просто стильно и дорого, они подчеркивают особый статус и престиж владельца. Кроме того, вы можете заказать индивидуальную гравировку на обратной стороне часов, которая сделает часы уникальными.
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>

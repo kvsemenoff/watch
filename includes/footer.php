297042869be9c30cb6153e6dc44b8582f962fa23
unknown <anzarsh@mail.ru>
2017-01-10 15:42:20 +0300

    add

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+<footer class="footer footer_padding footer_view">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="footer__left">
+					ООО «Твмарт-алм»<br>
+					г. Алматы, Санаторная улица, 44
+				</div>
+			</div>		
+			<div class="col-md-6">
+				<div class="footer__right">
+					Политика конфиденциальности
+				</div>
+			</div>
+		</div>
+	</div>
+	
+</footer>

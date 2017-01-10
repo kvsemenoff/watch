48a1855857ef228069f8ab81467caae3c96078aa
Barasbi <betokov93@gmail.com>
2017-01-10 17:49:37 +0300

    my

--- includes/what_good.php
+++ includes/what_good.php
@@ -1,7 +1,11 @@
 <section class="section section_padding section_padding-dc">
 	<center><h2>ЧЕМ ХОРОШИ ЧАСЫ КАЗАХСТАН</h2></center>
 
-	<div class="title-with-text">
+	<div class="image image-view">
+		<img class="dc-image" src="img/dc-clock.jpg">
+	</div>
+
+	<div class="title-with-text title-with-text_view">
 		<div class="title-with-text__title title-with-text__title_view ">Лимитированная серия часов</div>
 		<div class="title-with-text__text">
 			<p class="title-with-tex__p title-with-tex__p_view">Используются только качественные материалы и самые лучшие технологии обработки. Итальянская кожа ремешков обработана противоалергенным составом. 	Циферблат изготовлен методом штамповки, покрыт эмалью. 
@@ -14,5 +18,6 @@
 			</p>
 		</div>
 	</div>
+
 
 </section>

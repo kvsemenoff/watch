76828d7d97b8f6f051a3ed2363f679249f3fb35c
engine77 <zaur.ka.00@mail.ru>
2017-01-10 16:03:23 +0300

    10

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
 	<script src="js/common.js"></script>
 
 	<?php require_once('includes/header.php'); ?>
-	
+	<?php require_once('includes/clocks.php'); ?>
 	<?php require_once('includes/footer.php'); ?>
 
 	

6ff19450ab2b66c4a7f6255d82b156d834d58606
Konstantin Semenov <kvsemenoff@mail.ru>
2017-01-11 14:39:47 +0300

    d

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	<link rel="stylesheet" href="css/style.css">
 
 </head>
-<body class="pxl">
+<body class="pxl-1">
 	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
 	<script src="libs/owl.carousel/owl.carousel.js"></script>
 	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>

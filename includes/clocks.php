b1cedb5112b4f785fd1a8278ed79ef50a4c70128
engine77 <zaur.ka.00@mail.ru>
2017-01-11 11:39:42 +0300

    11

--- includes/clocks.php
+++ includes/clocks.php
@@ -1,61 +1,106 @@
 <section class="section section_padding section_view-df">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-
-					<h2 class="df-caption_view">Выбери свои часы Казахстан</h2>
-
-					<div class="df-clock df-clock_view-align">
-						<div class="df-clock__left df-clock__left_view">
-							<img class="df-clock__img img-responsive" src="img/df-clock1.png" alt="">
-						</div>
-						<div class="df-clock__right df-clock__right_view">
-							<span class="df-clock__cap df-clock__cap_view">Часы "Казахстан" <br> в черном цвете</span>
-							<span class="df-clock__old-price df-clock__old-price_view">27 980 тенге</span>
-							<span class="df-clock__new-price df-clock__new-price_view">13 990 тенге</span>
-							
-							<div class="df-clock__characteristic df-clock__characteristic_view">
-								<span class="df-clock__char1 df-clock__char1_view"></span>
-								<span class="df-clock__char2 df-clock__char2_view">46 мм</span>
-								<span class="df-clock__char3 df-clock__char3_view">3 атм</span>
-							</div>
-
-							<div class="characteristics characteristics_view">
-								<div class="characteristics__item characteristics__item_view">
-									<div class="characteristics__left characteristics__left_view">Тип механизма</div>
-									<div class="characteristics__right characteristics__right_view">Кварцевые</div>
-								</div>
-								<div class="characteristics__item characteristics__item_view">
-									<div class="characteristics__left characteristics__left_view">Материал корпуc</div>
-									<div class="characteristics__right characteristics__right_view">Нерж. сталь</div>
-								</div>
-								<div class="characteristics__item characteristics__item_view">
-									<div class="characteristics__left characteristics__left_view">Покрытие корпуса</div>
-									<div class="characteristics__right characteristics__right_view">Позолота</div>
-								</div>
-								<div class="characteristics__item characteristics__item_view">
-									<div class="characteristics__left characteristics__left_view">Размер корпуса</div>
-									<div class="characteristics__right characteristics__right_view">46 мм</div>
-								</div>
-								<div class="characteristics__item characteristics__item_view">
-									<div class="characteristics__left characteristics__left_view">Высота корпуса</div>
-									<div class="characteristics__right characteristics__right_view">14,6 мм</div>
-								</div>
-								<div class="characteristics__item characteristics__item_view">
-									<div class="characteristics__left characteristics__left_view">Стекло</div>
-									<div class="characteristics__right characteristics__right_view">Минеральное</div>
-								</div>
-								<div class="characteristics__item characteristics__item_view">
-									<div class="characteristics__left characteristics__left_view">Ремень </div>
-									<div class="characteristics__right characteristics__right_view">Кожа</div>
-								</div>
-							</div>
-							<a href="" class="df-clock__link df-clock__link_view button-1">Заказать</a>
-						</div>
-					</div>					
+	<div class="container df-container">
+		<h2 class="df-caption_view">Выбери свои часы Казахстан</h2>
+		<div class="df-clock df-clock_view-align">
+			<div class="df-clock__left df-clock__left_view">
+				<img class="df-clock__img df-img-absolute img-responsive" src="img/df-clock1.png" alt="">							
+			</div>
 			
+			<div class="df-clock__right df-clock__right_view">
+				<span class="df-clock__cap df-clock__cap_view">Часы "Казахстан" <br> в черном цвете</span>
+				<span class="df-clock__old-price df-clock__old-price_view">27 980 тенге</span>
+				<span class="df-clock__new-price df-clock__new-price_view">13 990 тенге</span>
+				
+				<div class="df-clock__characteristic df-clock__characteristic_view">
+					<span class="df-clock__char1 df-clock__char1_view"></span>
+					<span class="df-clock__char2 df-clock__char2_view">46 мм</span>
+					<span class="df-clock__char3 df-clock__char3_view">3 атм</span>
+				</div>
 
-			</div>	
+				<div class="characteristics characteristics_view">
+					<div class="characteristics__item characteristics__item_view">
+						<div class="characteristics__left characteristics__left_view">Тип механизма</div>
+						<div class="characteristics__right characteristics__right_view">Кварцевые</div>
+					</div>
+					<div class="characteristics__item characteristics__item_view">
+						<div class="characteristics__left characteristics__left_view">Материал корпуc</div>
+						<div class="characteristics__right characteristics__right_view">Нерж. сталь</div>
+					</div>
+					<div class="characteristics__item characteristics__item_view">
+						<div class="characteristics__left characteristics__left_view">Покрытие корпуса</div>
+						<div class="characteristics__right characteristics__right_view">Позолота</div>
+					</div>
+					<div class="characteristics__item characteristics__item_view">
+						<div class="characteristics__left characteristics__left_view">Размер корпуса</div>
+						<div class="characteristics__right characteristics__right_view">46 мм</div>
+					</div>
+					<div class="characteristics__item characteristics__item_view">
+						<div class="characteristics__left characteristics__left_view">Высота корпуса</div>
+						<div class="characteristics__right characteristics__right_view">14,6 мм</div>
+					</div>
+					<div class="characteristics__item characteristics__item_view">
+						<div class="characteristics__left characteristics__left_view">Стекло</div>
+						<div class="characteristics__right characteristics__right_view">Минеральное</div>
+					</div>
+					<div class="characteristics__item characteristics__item_view">
+						<div class="characteristics__left characteristics__left_view">Ремень </div>
+						<div class="characteristics__right characteristics__right_view">Кожа</div>
+					</div>
+				</div>
+				<a href="" class="df-clock__link df-clock__link_view">Заказать</a>
+			</div>
+			<div class="clear"></div>	
 		</div>
+		<div class="df-clock df-clock_view-align df-margin">
+			<div class="df-clock__left df-clock__left_view">
+				<img class="df-clock__img df-img-absolute img-responsive" src="img/df-clock2.png" alt="">							
+			</div>
+			
+			<div class="df-clock__right df-clock__right_view">
+				<span class="df-clock__cap df-clock__cap_view">Часы "Казахстан" <br> в черном цвете</span>
+				<span class="df-clock__old-price df-clock__old-price_view">27 980 тенге</span>
+				<span class="df-clock__new-price df-clock__new-price_view">13 990 тенге</span>
+				
+				<div class="df-clock__characteristic df-clock__characteristic_view">
+					<span class="df-clock__char1 df-clock__char1_view"></span>
+					<span class="df-clock__char2 df-clock__char2_view">46 мм</span>
+					<span class="df-clock__char3 df-clock__char3_view">3 атм</span>
+				</div>
+
+				<div class="characteristics characteristics_view">
+					<div class="characteristics__item characteristics__item_view">
+						<div class="characteristics__left characteristics__left_view">Тип механизма</div>
+						<div class="characteristics__right characteristics__right_view">Кварцевые</div>
+					</div>
+					<div class="characteristics__item characteristics__item_view">
+						<div class="characteristics__left characteristics__left_view">Материал корпуc</div>
+						<div class="characteristics__right characteristics__right_view">Нерж. сталь</div>
+					</div>
+					<div class="characteristics__item characteristics__item_view">
+						<div class="characteristics__left characteristics__left_view">Покрытие корпуса</div>
+						<div class="characteristics__right characteristics__right_view">Позолота</div>
+					</div>
+					<div class="characteristics__item characteristics__item_view">
+						<div class="characteristics__left characteristics__left_view">Размер корпуса</div>
+						<div class="characteristics__right characteristics__right_view">46 мм</div>
+					</div>
+					<div class="characteristics__item characteristics__item_view">
+						<div class="characteristics__left characteristics__left_view">Высота корпуса</div>
+						<div class="characteristics__right characteristics__right_view">14,6 мм</div>
+					</div>
+					<div class="characteristics__item characteristics__item_view">
+						<div class="characteristics__left characteristics__left_view">Стекло</div>
+						<div class="characteristics__right characteristics__right_view">Минеральное</div>
+					</div>
+					<div class="characteristics__item characteristics__item_view">
+						<div class="characteristics__left characteristics__left_view">Ремень </div>
+						<div class="characteristics__right characteristics__right_view">Кожа</div>
+					</div>
+				</div>
+				<a href="" class="df-clock__link df-clock__link_view">Заказать</a>
+			</div>
+			<div class="clear"></div>	
+		</div>					
+			
 	</div>
 </section>

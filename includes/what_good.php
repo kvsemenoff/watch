cca34cd749f8de6c50c23b4727527944b08a31a1
Barasbi <betokov93@gmail.com>
2017-01-11 09:44:03 +0300

    my

--- includes/what_good.php
+++ includes/what_good.php
@@ -1,7 +1,12 @@
 <section class="section section_padding section_padding-dc">
 	<center><h2>ЧЕМ ХОРОШИ ЧАСЫ КАЗАХСТАН</h2></center>
 
-	<div class="image image-view">
+    <div class="blue-view">
+        <div class="blue2-view">
+        	
+        </div>
+    </div>
+	<div class="image image-view">	    
 		<img class="dc-image" src="img/dc-clock.jpg">
 	</div>
 
@@ -10,9 +15,11 @@
 		<div class="title-with-text__text">
 			<p class="title-with-tex__p title-with-tex__p_view">Используются только качественные материалы и самые лучшие технологии обработки. Итальянская кожа ремешков обработана противоалергенным составом. 	Циферблат изготовлен методом штамповки, покрыт эмалью. 
 			</p>
+			<br>
 			<p class="title-with-text__p title-with-tex__p_view">
 				Корпус часов и накладные знаки циферблата покрыты настоящей позолотой, что не только практично, но и очень эффектно выглядит. Стекло минеральное, устойчивое к механическим воздействиям.
 			</p>
+			<br>
 			<p class="title-with-text__p title-with-tex__p_view">
 				Часы коллекции "Казахстан" прослужат долгие годы и никогда не потеряют прекрасный внешний вид!
 			</p>
